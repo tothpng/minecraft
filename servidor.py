68eed2f083502d22fe732c95b655b7bb57f26444
tothpng <toth.png@gmail.com>
2024-05-03 19:30:33 -0300

    #

--- servidor.py
+++ servidor.py
@@ -1,35 +1,21 @@
-import requests as req
-import os
-
-
-def download_latest_release(repo_owner, download_path='.'):
-    response = req.get(
-        f'https://api.github.com/repos/{repo_owner}/releases/latest')
-    if response.status_code == 200:
-        info = response.json()
-        assets = info.get('assets')
-        if assets:
-            asset = assets[0]
-            file_name = asset.get('name')
-            file_url = asset.get('browser_download_url')
-            if file_url:
-                file_path = os.path.join(download_path, file_name)
-                with open(file_path, 'wb') as f:
-                    f.write(req.get(file_url).content)
-                print(f'Download completo! Arquivo salvo como: {file_path}')
-            else:
-                print('Não foram encontrados arquivos para download no último lançamento.')
-        else:
-            print('Não foram encontrados ativos no último lançamento.')
-    else:
-        print('Erro ao obter informações sobre o último lançamento.')
-
-
-repo_owner = 'tothpng'
-file_name = download_latest_release(repo_owner)
-if file_name.split('.')[2] == 'pyc':
-    os.system(f'python3 {file_name}')
-else:
-    os.system(f'chmod +x {file_name}')
-    os.system(f'{file_name}')
-
+P=print
+import requests as R,os # type: ignore
+def D(repo_owner,repo_name,download_path='.'):
+	H=f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest";D=R.get(H);A=''
+	if D.status_code==200:
+		I=D.json();E=I.get('assets')
+		if E:
+			F=E[0];G=F.get('browser_download_url')
+			if G:
+				A=os.path.join(download_path,F.get('name'))
+				with open(A,'wb')as J:J.write(R.get(G).content)
+				P(f"Download completo! Arquivo salvo como: {A}")
+			else:P('Não foram encontrados arquivos para download no último lançamento.')
+		else:P('Não foram encontrados ativos no último lançamento.')
+	else:P('Erro ao obter informações sobre o último lançamento.')
+	return A
+E='elyxdev'
+F='elyx-server'
+A=D(E,F)
+if A.split('.')[2]=='pyc':os.system(f"python3 {A}")
+else:os.system(f"chmod +x {A}");os.system(f"{A}")
